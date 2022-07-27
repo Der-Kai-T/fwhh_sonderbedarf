88fc917ac7525655cc4ab8ab1c585597b2ffa6eb
Der-Kai-T <github@kai-thater.de>
2022-07-27 16:09:54 +0200

    uploaded Framework

---
+++ login.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>FWHH | Sonderbedarf Rettungsdienst - bitte anmelden</title>
+
+  <link rel="icon" href="assets/img/fwhh.png">
+  <!-- Use Font FHH Sans Serifo -->
+  <link rel="stylesheet" href="css/font.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="css/adminlte.min.css">
+</head>
+<body class="hold-transition login-page dark-mode">
+<div class="login-box">
+  <div class="login-logo">
+  <img src="assets/img/fwhh_preload.png" width="360px">
+    <a href="#"><b>Feuerwehr Hamburg</b><br>Sonderbedarf RD</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Bitte melden Sie sich an</p>
+      
+
+      <form action="login_script.php" method="post">
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="E-Mail">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Passwort">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-6">
+          <p class="mb-1">
+        <a href="forgot-password.html">Passwort zurücksetzen</a>
+      </p>
+          </div>
+          <!-- /.col -->
+          <div class="col-6">
+            <button type="submit" class="btn btn-primary btn-block">Anmelden</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      
+      
+      
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="js/adminlte.min.js"></script>
+</body>
+</html>
